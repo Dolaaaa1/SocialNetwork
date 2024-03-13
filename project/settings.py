040dcfb226f7d64db88f51a93d9667cf4788d3dd
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-03-13 15:56:33 +0200

    add Authentication(sign up ,login ,logout

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'crispy_forms',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['Templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -114,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = "static/"
 
